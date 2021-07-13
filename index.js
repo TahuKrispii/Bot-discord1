8ecea35816176ce269a715d120dc564d821bce89
TahuKrispii <87348844+TahuKrispii@users.noreply.github.com>
2021-07-13 09:28:00 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on("ready", async () => {
+  console.log('${user.tag}')
+  client.user.setActivity("Mangan Tahu Krispi")
+})
+
+client.login("ODU4NzI3OTU1NTAzOTA2ODY2.YNiWug.8eQiCACKcrfenFZIX275T6XZMNY")
