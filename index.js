5c1180a0a78aec57b5f27677ad3b3a6e4b4fb084
TahuKrispii <87348844+TahuKrispii@users.noreply.github.com>
2022-04-05 09:30:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client()
 
 client.on("ready", async () => {
   console.log('${user.tag}')
-  client.user.setActivity("Mangan Tahu Krispi")
+  client.user.setActivity("Tahu Bang")
 })
 
 client.login("ODU4NzI3OTU1NTAzOTA2ODY2.YNiWug.8eQiCACKcrfenFZIX275T6XZMNY")
